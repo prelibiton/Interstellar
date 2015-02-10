8ace9d607e373d1526f127d9155ff7d3dc763fe0
prelibiton <rainship@gmail.com>
2015-02-10 23:00:32 +0200

    new asteroids added

--- lib/asteroid.rb
+++ lib/asteroid.rb
@@ -8,12 +8,13 @@ class Asteroid
 
   def initialize
     load_images
-    @asteroid = [@asteroid_01, @asteroid_02, @asteroid_03, @asteroid_04].shuffle.first
+    @asteroid = [@asteroid_01, @asteroid_02, @asteroid_03, @asteroid_04,
+      @asteroid_05,@asteroid_06].shuffle.first
     @x = rand(RANGE)
     @y = 0
     @speedx = rand(MAX_SPEED_X * 2) - MAX_SPEED_X
     @speedy = rand(MAX_SPEED_Y - MIN_SPEED_Y) + MIN_SPEED_Y
-    @radius = 40
+    @radius = 30
     @size = 1
   end
 
@@ -37,9 +38,11 @@ class Asteroid
     @asteroids = Gosu::TexturePacker.load_json(
       $window, "media/asteroids.json", :precise)
     @asteroid_01 = @asteroids.frame('meteorGrey_big4.png')
-    @asteroid_02 = @asteroids.frame('meteorGrey_big2.png')
-    @asteroid_03 = @asteroids.frame('meteorBrown_big2.png')
-    @asteroid_04 = @asteroids.frame('meteorBrown_big4.png')
+    @asteroid_02 = @asteroids.frame('meteorBrown_big4.png')
+    @asteroid_03 = @asteroids.frame('meteorBrown_med1.png')
+    @asteroid_04 = @asteroids.frame('meteorGrey_med1.png')
+    @asteroid_05 = @asteroids.frame('meteorBrown_small1.png')
+    @asteroid_06 = @asteroids.frame('meteorGrey_med2.png')
   end
 
   
