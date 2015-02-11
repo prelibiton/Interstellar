2f3cacb08e482984fc5d8ab1d71fadc91ea09e6d
prelibiton <rainship@gmail.com>
2015-02-11 22:49:15 +0200

    spec_helper description

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ require_relative '../lib/numerical'
 require_relative '../lib/asteroid'
 require_relative '../lib/enemy'
 
+# Before writing rspec spec to the console, 
+# make sure that all of the load_images are 
+# comments in the initialize method.Otherwise 
+# you will get an error.
+
